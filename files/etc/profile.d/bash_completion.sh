79adfaca73790f585720da990d48ca99e0820060
Mick Pollard <261833+aussielunix@users.noreply.github.com>
2024-02-03 08:02:03 +1100

    bringing in brew completions from upstream

--- files/etc/profile.d/bash_completion.sh
+++ files/etc/profile.d/bash_completion.sh
@@ -1,6 +1,7 @@
 # shellcheck shell=sh disable=SC1091,SC2039,SC2166
 # Check for interactive bash and that we haven't already been sourced.
 if [ "x${BASH_VERSION-}" != x -a "x${PS1-}" != x -a "x${BASH_COMPLETION_VERSINFO-}" = x ]; then
+
     # Check for recent enough version of bash.
     if [ "${BASH_VERSINFO[0]}" -gt 4 ] ||
         [ "${BASH_VERSINFO[0]}" -eq 4 -a "${BASH_VERSINFO[1]}" -ge 2 ]; then
@@ -13,6 +14,17 @@ if [ "x${BASH_VERSION-}" != x -a "x${PS1-}" != x -a "x${BASH_COMPLETION_VERSINFO
         if shopt -q progcomp && [ -r /usr/local/share/bash-completion/bash_completion ]; then
             # Source completion code.
             . /usr/local/share/bash-completion/bash_completion
+        fi
+        if ! test -L /home/linuxbrew/.linuxbrew/etc/bash_completion.d/brew; then
+            /home/linuxbrew/.linuxbrew/bin/brew completions link
+        fi
+        if test -d /home/linuxbrew/.linuxbrew/etc/bash_completion.d; then
+            for rc in /home/linuxbrew/.linuxbrew/etc/bash_completion.d/*; do
+              if test -r "$rc"; then
+                . "$rc"
+              fi
+            done
+            unset rc
         fi
     fi
 fi
